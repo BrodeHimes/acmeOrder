e8b71ab80955a3dae8c0966ab517baf1839ade70
BrodeHimes <jaimegonznv@gmail.com>
2020-10-14 14:08:58 -0700

    cleaning up whitespace

--- index.js
+++ index.js
@@ -1,11 +1,8 @@
-
 //Tabing Through Order Defaults Function
-
 let billingTab = document.getElementById('billingTab');
 let shippingTab = document.getElementById('shippingTab');
 let paymentTab = document.getElementById('paymentTab');
 let tabButtons = document.querySelectorAll('section.tabNames > button');
-
 let hiddenTabClass ='orderDefaultTabs border hidden';
 let visibleTabClass = 'orderDefaultTabs border';
 
@@ -17,15 +14,10 @@ let clickedBilling = ()=>{
     tabButtons[1].setAttribute('tabindex','-1');
     tabButtons[2].setAttribute('tabindex','-1');
     tabButtons[0].setAttribute('tabindex','0');
-
-    
     tabButtons[0].setAttribute('id','focusedTab');
     tabButtons[1].removeAttribute('id', 'focusedTab');
     tabButtons[2].removeAttribute('id','focusedTab');
-
     tabClicked = 'billing';
-    
-
 }
 tabButtons[0].addEventListener('click',clickedBilling)
 
@@ -36,11 +28,9 @@ let clickedShipping = ()=>{
     tabButtons[0].setAttribute('tabindex','-1');
     tabButtons[1].setAttribute('tabindex','0');
     tabButtons[2].setAttribute('tabindex','-1');
-
     tabButtons[0].removeAttribute('id', 'focusedTab');
     tabButtons[1].setAttribute('id','focusedTab');
     tabButtons[2].removeAttribute('id','focusedTab');
-
     tabClicked = 'shipping';
 }
 tabButtons[1].addEventListener('click',clickedShipping);
@@ -52,17 +42,13 @@ let clickedPayment = ()=>{
     tabButtons[0].setAttribute('tabindex','-1');
     tabButtons[1].setAttribute('tabindex','-1');
     tabButtons[2].setAttribute('tabindex','0');
-
     tabButtons[0].removeAttribute('id', 'focusedTab');
     tabButtons[1].removeAttribute('id','focusedTab');
     tabButtons[2].setAttribute('id','focusedTab');
-
     tabClicked = 'payment';
 }
 tabButtons[2].addEventListener('click',clickedPayment);
 
-
- 
 //Switching though tabs via arrow keys
 let switchTabsFromBilling=()=>{
     switch (event.key) {
@@ -74,7 +60,6 @@ let switchTabsFromBilling=()=>{
         clickedShipping()
         tabButtons[1].focus();
         break;
-        
     }
 }
 
@@ -104,22 +89,9 @@ let switchTabsFromPayment=()=>{
     }
 }
 
-
-
 tabButtons[0].addEventListener('keydown',switchTabsFromBilling)
 tabButtons[1].addEventListener('keydown',switchTabsFromShipping)
 tabButtons[2].addEventListener('keydown',switchTabsFromPayment)
-
- 
-
-
-
-
-
-
-
-
-
 
 // VIP Modal
 let moreDetailsLink = document.querySelector('aside > a');
@@ -141,39 +113,34 @@ let showVIPModal=()=>{
     headerContainer[0].setAttribute('aria-hidden','true');
     footerContainer[0].setAttribute('aria-hidden','true');
     asideContainer[0].setAttribute('aria-hidden','true');
-
     for(i=0;i<inputs.length;i++){
-        inputs[i].setAttribute('tabindex','-1')
+      inputs[i].setAttribute('tabindex','-1')
     };
     for(i=0;i<buttons.length;i++){
-        buttons[i].setAttribute('tabindex','-1')
+      buttons[i].setAttribute('tabindex','-1')
     }
     for(i=0;i<links.length;i++){
-        links[i].setAttribute('tabindex','-1')
+      links[i].setAttribute('tabindex','-1')
     };
-    
     for(i=0;i<modalLinks.length;i++){
-        modalLinks[i].setAttribute('tabindex','0')
+      modalLinks[i].setAttribute('tabindex','0')
     };
-
     modalLinks[0].focus();
-
     let overlay= document.createElement('div');
     overlay.setAttribute('id','overlay');
     document.body.insertAdjacentElement('afterbegin',overlay);
-
     document.addEventListener('keydown',escapeClose);
-
 }
 
 moreDetailsLink.addEventListener('click',showVIPModal);
-
+// close modal w/ escape key
 let escapeClose =()=>{
     if (event.keyCode===27){
         closeVIPModal();
     }
 }
 
+//close modal
 let closeVIPModal=()=>{
     vipModal[0].className="vipModal border hidden";
     mainContainer[0].removeAttribute('aria-hidden');
@@ -183,7 +150,6 @@ let closeVIPModal=()=>{
     billingTab.className=visibleTabClass;
     shippingTab.className=hiddenTabClass;
     paymentTab.className=hiddenTabClass;
-    
     for(i=0;i<inputs.length;i++){
         inputs[i].removeAttribute('tabindex')
     };
@@ -193,24 +159,18 @@ let closeVIPModal=()=>{
     for(i=0;i<links.length;i++){
         links[i].removeAttribute('tabindex')
     };
-    
     for(i=0;i<modalLinks.length;i++){
         modalLinks[i].removeAttribute('tabindex')
     };
-
-
     keepClickedOrderTabOpen();
-    
     let overlay=document.getElementById('overlay')
     document.body.removeChild(overlay);
-
     moreDetailsLink.focus();
 }
 
 closeModalButton.addEventListener('click',closeVIPModal);
  
 // Ensures the previously clicked Order Default tab stays opened when you close vip modal
-
 let keepClickedOrderTabOpen=()=>{
     if (tabClicked=='billing'){
      clickedBilling();
@@ -221,26 +181,16 @@ let keepClickedOrderTabOpen=()=>{
     }
 }
 
-
-
-
-
-//VALIDATION FUNCTIONS
-
-//billing Validation
+//FORM VALIDATION 
 let billingErrorMessage = document.querySelectorAll('#billingForm .error');
 let billingInputBoxes = document.querySelectorAll('section form#billingform input');
 let billingForm = document.getElementById('billingForm');
-// Shipping Validation
 let shippingInputBoxes=document.querySelectorAll('section form#shippingForm input');
 let shippingErrorMessage = document.querySelectorAll('#shippingForm .error');
 let shippingForm = document.getElementById('shippingForm');
-// Payment Validation
 let paymentInputBoxes=document.querySelectorAll('section form#paymentForm input');
 let paymentErrorMessage = document.querySelectorAll('#paymentForm .error');
 let paymentForm = document.getElementById('paymentForm');
-
-
 
 let mainFunction = ()=>{
     if (event.target.id=='billingForm'){
@@ -251,7 +201,6 @@ let mainFunction = ()=>{
         validationFunction(paymentInputBoxes,paymentErrorMessage)
     }
 };
-
 
 let validationFunction =(inputBoxesToCheck,errorMessageToShow)=>{
     for (i=0;i<inputBoxesToCheck.length;i++){
@@ -278,7 +227,3 @@ let stopError = (i,inputBoxesToCheck,errorMessageToShow)=>{
 billingForm.addEventListener('submit',mainFunction);
 shippingForm.addEventListener('submit',mainFunction);
 paymentForm.addEventListener('submit',mainFunction);
-
-
-
- 
